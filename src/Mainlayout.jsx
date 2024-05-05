4d4cedf3983194f8d314ff11bec9c8bcda0a5cf6
rameshinumula <inumularamesh@gmail.com>
2024-05-05 09:36:19 +0530

    new route all products

--- src/Mainlayout.jsx
+++ src/Mainlayout.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import TopBar from './Topbar/TopBar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Home/Home'
+import AllProducts from './Products/AllProducts';
 
-export default function Mainlayout() {
+export default function Mainlayout(props) {
+  const { history } = props;
   return (
     <div>
-        <TopBar />
+        <TopBar history={history} />
         <Routes>
             <Route path='/' element={<Home />}></Route>
+            <Route path='/viewProducts' element={<AllProducts />}></Route>
         </Routes>
     </div>
   )
