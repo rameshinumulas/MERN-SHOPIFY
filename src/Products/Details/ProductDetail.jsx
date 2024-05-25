1f80040a8d0863f2f6814d3880fbbcd136318c2d
Lokeshjana <88444167+Lokeshjana@users.noreply.github.com>
2024-05-25 23:10:15 +0530

    product details page ui

--- src/Products/Details/ProductDetail.jsx
+++ src/Products/Details/ProductDetail.jsx
@@ -2,10 +2,17 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getProductById } from '../../redux/actions';
 import { useParams } from 'react-router-dom';
+import RatingComp from '../../commonComp/RatingComp';
+import NumberFormatCom from '../../commonComp/NumberFormatCom';
 
 export default function ProductDetail(props) {
   // const { match: { params: { id } } } = props;
   const { id } = useParams();
+  const getDiscountPrice = (listPrice, discount) => {
+    let discountPrice = (discount / 100) * listPrice;
+    const totlaPrice = discountPrice + listPrice;
+    return totlaPrice;
+  }
   const dispatch = useDispatch();
   const { productDetailsById } = useSelector(state => state);
   console.log(productDetailsById, 'dd---');
@@ -26,10 +33,63 @@ export default function ProductDetail(props) {
           </div>
         </div>
       </div>
-      <div className='col-6'>
-
+      <div className='col-4'>
+        <p className="h2">
+          {productDetailsById?.title}
+        </p>
+        <p>
+          <RatingComp value={productDetailsById?.rating} />
+        </p>
+        <NumberFormatCom
+          value={productDetailsById?.price}
+          prefix="$"
+          thousandSeparator={true}
+          displayType={'text'}
+          isBold
+        />
+        <div style={{ display: 'flex' }}>
+          <NumberFormatCom
+            value={parseFloat(getDiscountPrice(productDetailsById?.price, productDetailsById?.discountPercentage)).toFixed(0)}
+            prefix="$"
+            thousandSeparator={true}
+            displayType={'text'}
+            strikeOut
+            isBold
+          />
+          <NumberFormatCom
+            value={productDetailsById?.discountPercentage}
+            prefix=""
+            thousandSeparator={true}
+            displayType={'text'}
+            suffix='%'
+          />
+        </div>
+        <div className='row'>
+          <div className='col-3'>
+            <p className='fw-bold m-0'>Brand</p>
+            <p className='fw-bold m-0'>Category</p>
+          </div>
+          <div className='col-5'>
+            <p className='fw-normal m-0'>{productDetailsById?.brand}</p>
+            <p className='fw-normal m-0'>{productDetailsById?.category}</p>
+          </div>
+        </div>
+        <div className='row mt-3'>
+          <div className='col'>
+            <p className='fw-bold m-0'>About the product</p>
+            <p className='fw-normal'>
+              {productDetailsById?.description}
+            </p>
+          </div>
+        </div>
+        {/* <div style={{ display: 'flex', flexDirection: 'row' }}>
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+            </div> */}
       </div>
-      Details used
+      <div className='col-4'>
+        <p className='h2'>Reviews</p>
+      </div>
     </div>
   )
 }
