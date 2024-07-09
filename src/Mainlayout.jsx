91b97658d17ed34d326dac3e64b5bb451f8ba2bf
ramesh inumula <inumularamesh@gmail.com>
2024-07-09 22:20:57 +0530

    view cart route

--- src/Mainlayout.jsx
+++ src/Mainlayout.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './Home/Home'
 import AllProducts from './Products/AllProducts';
 import ProductDetail from './Products/Details/ProductDetail';
+import ViewCart from './OrderRalated/ViewCart';
 
 export default function Mainlayout(props) {
   return (
@@ -13,6 +14,7 @@ export default function Mainlayout(props) {
           <Route path='/' element={<Home />}></Route>
           <Route path='/viewProducts' element={<AllProducts />}></Route>
           <Route path='/product/:id/view' element={<ProductDetail />}></Route>
+          <Route path='/viewcart' element={<ViewCart />}></Route>
         </Routes>
     </div>
   )
